eb49c1267ba06b2889bcfedc72f4aec8cffab0fd
Oleg <olegjan9@rambler.ru>
2022-06-15 08:34:46 +0300

    14.06.2022

--- src/AppFilterProducts/AppFilterProducts.js
+++ src/AppFilterProducts/AppFilterProducts.js
@@ -5,54 +5,17 @@ import { Component } from "react"
 
 
 class AppFilterProducts extends Component {
-    state = {
-        liData: [
-            {name: "NordicTrack C100"},
-            {name: "Clear Fit Luxury LT.20"},
-            {name: "Ammity ATM 720 TFT+"}
-        ],
-
-        checkboxData: [
-            {idH2: "headingOne", id: "collapseOne", dataBsTarget: "#collapseOne", ariaExpanded: "true", ariaControls: "collapseOne", class: "accordion-collapse", ariaLabelledby: "headingOne"},
-            {idH2: "headingTwo", id: "collapseTwo", dataBsTarget: "#collapseTwo", ariaExpanded: "true", ariaControls: "collapseTwo", class: "accordion-collapse", ariaLabelledby: "headingTwo"},
-            {idH2: "headingThree", id: "collapseThree", dataBsTarget: "#collapseThree", ariaExpanded: "true", ariaControls: "collapseThree", class: "accordion-collapse", ariaLabelledby: "headingThree"},          
-        ]
-    }
-    onDelLiData = (name) => {
-        this.setState({
-            liData: this.state.liData.filter(item=> {
-                return item.name !== name                      
-            })
-        })      
-    }
+    // state = {
+    //     liData: [
+    //         {name: "NordicTrack C100"},
+    //         {name: "Clear Fit Luxury LT.20"},
+    //         {name: "Ammity ATM 720 TFT+"}
+    //     ],
+    // }
+    
 
     render(){
-        const li = this.state.liData.map(({name}) => {
-            return(
-                <li key = {name} className="items-nav__product">
-                    <a href="#" className="items-nav__item">{name}</a>
-                    <div onClick={(e)=>{this.onDelLiData(name)}} className="items-nav__close"></div>                            
-                </li> 
-            )
-        })
-
-        const accordion = this.state.checkboxData.map((item) => {
-            return(
-                <div class="accordion-item">
-                    <h2 class="accordion-header" id={item.idH2}>
-                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                            Accordion Item #1
-                        </button>
-                    </h2>
-                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                        <div class="accordion-body">
-                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                        </div>
-                    </div>
-            </div>
-            )
-        }) 
-        
+        const {liData} = this.props   
         return(
             <section className="AppFilterProducts"> 
                 <div className="_container">            
@@ -61,13 +24,56 @@ class AppFilterProducts extends Component {
                         <div className="nav__item items-nav">
                             <div className="items-nav__title">Товары для сравнения</div>
                                 <ul className="items-nav__wrapper">
-                                    {li}                              
+                                    {liData}                              
                                 </ul>                                              
                         </div>
-                        <div class="accordion" id="accordionExample">
+                        <div className="accordion" id="accordionExample">
                             
                         </div>
                     </nav>
+                </div>
+
+                <div className="accordion" id="accordionExample">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                            <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                Accordion Item #1
+                            </button>
+                        </h2>
+                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+
+
+
+                    
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingTwo">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Accordion Item #2
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingThree">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                        </h2>
+                        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
                 </div>
 
                 
--- src/AppOne/AppOne.js
+++ src/AppOne/AppOne.js
@@ -1,14 +1,52 @@
 import './AppOne.css';
 import AppOneHeader from "../AppOneHeader/AppOneHeader"
 import AppFilterProducts from "../AppFilterProducts/AppFilterProducts"
+import {Component} from "react"
 
-function AppOne() {
-  return (
-    <>
-    <AppOneHeader/>
-    <AppFilterProducts/>
-   </>
-  );
+class AppOne extends Component {
+  state = {
+    liData: [
+        {name: "NordicTrack C100"},
+        {name: "Clear Fit Luxury LT.20"},
+        {name: "Ammity ATM 720 TFT+"}
+    ],
+    accordion: [
+      {show: "show", collapsed: "collapsed"}
+    ],
+  }
+
+  onDelLiData = (name) => {
+    this.setState({
+        liData: this.state.liData.filter(item=> {
+            return item.name !== name                      
+        })
+    })     
+  }
+
+  changeClassAccordion = () => {
+    this.setState({
+      show: this.state
+    })
+  }  
+  
+  render(){
+    const li = this.state.liData.map(({name}) => {
+      return(
+          <li key = {name} className="items-nav__product">
+              <a href="#" className="items-nav__item">{name}</a>
+              <div onClick={(e)=>{this.onDelLiData(name)}} className="items-nav__close"></div>                            
+          </li> 
+      )
+  })
+    return (
+      <>
+      <AppOneHeader/>
+      <AppFilterProducts
+        liData = {li}
+        state = {this.state.liData}/>
+    </>
+    );
+  }
 }
 
 export default AppOne;
